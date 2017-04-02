422ab7a187e64dc6cd5ae291a3bcf1d0f1600537
kwonheejeong <gmlwjd9405@naver.com>
2017-04-02 17:48:18 +0900

    add addProduct.jsp We have not yet implemented the method for saving the data entered by the user in db.

--- eStore/src/main/java/kr/ac/hansung/cse/controller/AdminController.java
+++ eStore/src/main/java/kr/ac/hansung/cse/controller/AdminController.java
@@ -26,141 +26,147 @@ import kr.ac.hansung.cse.service.ProductService;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	
+
 	@Autowired
 	private ProductService productService;
-	
+
 	@RequestMapping
 	public String adminPage() {
 		return "admin";
 	}
-	
-	//db를 조회해서 products list를 가져온 후 model에 저장해서 view에 전달한다.
+
+	// db를 조회해서 products list를 가져온 후 model에 저장해서 view에 전달한다.
 	@RequestMapping("/productInventory")
 	public String getProducts(Model model) {
 		List<Product> products = productService.getProducts();
 		model.addAttribute("products", products);
-		
+
 		return "productInventory";
 	}
-	
-//	
-//	@RequestMapping(value="/productInventory/addProduct", method=RequestMethod.GET)
-//	public String addProduct(Model model) {
-//		Product product = new Product();
-//		
-//		product.setName("노트북");
-//		product.setCategory("컴퓨터");
-//		model.addAttribute("product", product);
-//		
-//		return "addProduct";
-//	}
-//	
-//	@RequestMapping(value="/productInventory/addProduct", method=RequestMethod.POST)
-//	public String addProductPost(@Valid Product product, BindingResult result, HttpServletRequest request) {
-//		if(result.hasErrors()) {
-//			System.out.println("===From data has some errors===");
-//			List<ObjectError> errors = result.getAllErrors();
-//			for(ObjectError error:errors) {
-//				System.out.println(error.getDefaultMessage());
-//			}
-//			return "addProduct";
-//		}
-//		
-//		MultipartFile productImage = product.getProductImage();
-//		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-//		Path savePath = Paths.get(rootDirectory + "\\resources\\images\\" + productImage.getOriginalFilename());
-//		if(productImage != null && !productImage.isEmpty()) {
-//			try {
-//				productImage.transferTo(new File(savePath.toString()));
-//			}
-//			catch(Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		
-//		product.setImageFilename(productImage.getOriginalFilename());
-//		
-////		if(productImage.isEmpty() == false) {
-////			System.out.println("------ file start ------");
-////			System.out.println("name: " + productImage.getName());
-////			System.out.println("filename: " + productImage.getOriginalFilename());
-////			System.out.println("filrsize: " + productImage.getSize());
-////			System.out.println("------ file start ------");
-////		}
-////		
-//		if( !productService.addProduct(product)) {
-//			System.out.println("Adding product cannot be done");
-//		}
-//		
-//		return "redirect:/admin/productInventory";
-//	}
-//	
-//	@RequestMapping("/productInventory/deleteProduct/{id}")
-//	public String deleteProduct(@PathVariable int id, HttpServletRequest request) {
-//		
-//		Product product = productService.getProductById(id);
-//		
-//		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-//		Path path = Paths.get(rootDirectory + "\\resources\\images\\" + product.getImageFilename());
-//		
-//		if(Files.exists(path)) {
-//			try {
-//				Files.delete(path);
-//			}
-//			catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		
-//		
-//		if(! productService.deleteProductById(id)) {
-//			System.out.println("Deleting product cannot be done");
-//		}
-//		
-//		return "redirect:/admin/productInventory";
-//	}
-//	
-//	@RequestMapping("/productInventory/editProduct/{id}")
-//	public String editProduct(@PathVariable int id, Model model) {
-//		Product product = productService.getProductById(id);
-//		
-//		model.addAttribute("product", product);
-//
-//		return "editProduct";
-//	}
-//	
-//	@RequestMapping(value="/productInventory/editProduct", method=RequestMethod.POST)
-//	public String editProductPost(@Valid Product product, BindingResult result, HttpServletRequest request) {
-//		
-//		if(result.hasErrors()) {
-//			System.out.println("===From data has some errors===");
-//			List<ObjectError> errors = result.getAllErrors();
-//			for(ObjectError error:errors) {
-//				System.out.println(error.getDefaultMessage());
-//			}
-//			return "editProduct";
-//		}
-//		
-//		MultipartFile productImage = product.getProductImage();
-//		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-//		Path savePath = Paths.get(rootDirectory + "\\resources\\images\\" + productImage.getOriginalFilename());
-//		if(productImage != null && !productImage.isEmpty()) {
-//			try {
-//				productImage.transferTo(new File(savePath.toString()));
-//			}
-//			catch(Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		
-//		product.setImageFilename(productImage.getOriginalFilename());
-//		
-//		if(! productService.editProduct(product)) {
-//			System.out.println("Editing product cannot be done");
-//		}
-//
-//		return "redirect:/admin/productInventory";
-//	}
+
+	// products를 추가할 수 있도록 하는 페이지 구성.
+	// 여기의 model를 "addProcuct"view에서 참조하여 control form에 내용을 채워준다.
+	// 즉, 여기 model에 들어가 있는 객체와 나중에 view의 control들과 자동으로 matching된다.
+	@RequestMapping(value = "/productInventory/addProduct", method = RequestMethod.GET)
+	public String addProduct(Model model) {
+		// 반드시 하나 생성해서 넘겨 줘야함. (빈 객체라도 생성해야 함.)
+		// 나중에 view에서 spring form tag를 사용하는데 그때 서로 matching해야 하기 때문
+		Product product = new Product();
+
+		// 초기값 지정
+		product.setName("노트북");
+		product.setCategory("컴퓨터");
+		model.addAttribute("product", product);
+
+		return "addProduct";
+	}
+
+	//
+	// @RequestMapping(value="/productInventory/addProduct", method=RequestMethod.POST)
+	// public String addProductPost(@Valid Product product, BindingResult result, HttpServletRequest request) {
+	// if(result.hasErrors()) {
+	// System.out.println("===From data has some errors===");
+	// List<ObjectError> errors = result.getAllErrors();
+	// for(ObjectError error:errors) {
+	// System.out.println(error.getDefaultMessage());
+	// }
+	// return "addProduct";
+	// }
+	//
+	// MultipartFile productImage = product.getProductImage();
+	// String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+	// Path savePath = Paths.get(rootDirectory + "\\resources\\images\\" + productImage.getOriginalFilename());
+	// if(productImage != null && !productImage.isEmpty()) {
+	// try {
+	// productImage.transferTo(new File(savePath.toString()));
+	// }
+	// catch(Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+	//
+	// product.setImageFilename(productImage.getOriginalFilename());
+	//
+	//// if(productImage.isEmpty() == false) {
+	//// System.out.println("------ file start ------");
+	//// System.out.println("name: " + productImage.getName());
+	//// System.out.println("filename: " + productImage.getOriginalFilename());
+	//// System.out.println("filrsize: " + productImage.getSize());
+	//// System.out.println("------ file start ------");
+	//// }
+	////
+	// if( !productService.addProduct(product)) {
+	// System.out.println("Adding product cannot be done");
+	// }
+	//
+	// return "redirect:/admin/productInventory";
+	// }
+	//
+	// @RequestMapping("/productInventory/deleteProduct/{id}")
+	// public String deleteProduct(@PathVariable int id, HttpServletRequest request) {
+	//
+	// Product product = productService.getProductById(id);
+	//
+	// String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+	// Path path = Paths.get(rootDirectory + "\\resources\\images\\" + product.getImageFilename());
+	//
+	// if(Files.exists(path)) {
+	// try {
+	// Files.delete(path);
+	// }
+	// catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// }
+	//
+	//
+	// if(! productService.deleteProductById(id)) {
+	// System.out.println("Deleting product cannot be done");
+	// }
+	//
+	// return "redirect:/admin/productInventory";
+	// }
+	//
+	// @RequestMapping("/productInventory/editProduct/{id}")
+	// public String editProduct(@PathVariable int id, Model model) {
+	// Product product = productService.getProductById(id);
+	//
+	// model.addAttribute("product", product);
+	//
+	// return "editProduct";
+	// }
+	//
+	// @RequestMapping(value="/productInventory/editProduct", method=RequestMethod.POST)
+	// public String editProductPost(@Valid Product product, BindingResult result, HttpServletRequest request) {
+	//
+	// if(result.hasErrors()) {
+	// System.out.println("===From data has some errors===");
+	// List<ObjectError> errors = result.getAllErrors();
+	// for(ObjectError error:errors) {
+	// System.out.println(error.getDefaultMessage());
+	// }
+	// return "editProduct";
+	// }
+	//
+	// MultipartFile productImage = product.getProductImage();
+	// String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+	// Path savePath = Paths.get(rootDirectory + "\\resources\\images\\" + productImage.getOriginalFilename());
+	// if(productImage != null && !productImage.isEmpty()) {
+	// try {
+	// productImage.transferTo(new File(savePath.toString()));
+	// }
+	// catch(Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+	//
+	// product.setImageFilename(productImage.getOriginalFilename());
+	//
+	// if(! productService.editProduct(product)) {
+	// System.out.println("Editing product cannot be done");
+	// }
+	//
+	// return "redirect:/admin/productInventory";
+	// }
 
 }
--- eStore/src/main/webapp/WEB-INF/views/productInventory.jsp
+++ eStore/src/main/webapp/WEB-INF/views/productInventory.jsp
@@ -1,7 +1,7 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-
 <div class="container-wrapper">
 	<div class="container">
 		<h2>Product Inventory Page</h2>
@@ -9,26 +9,40 @@
 		<table class="table table-striped">
 			<thead>
 				<tr class="bg-success">
+					<th>Photo Thumb</th>
 					<th>Product Name</th>
 					<th>Category</th>
 					<th>Price</th>
 					<th>Manufacturer</th>
 					<th>UnitInStock</th>
 					<th>Description</th>
+					<th></th>
 				</tr>
 			</thead>
 			<tbody>
 				<c:forEach var="product" items="${products}">
 					<tr>
+						<td><img
+							src="<c:url value="/resources/images/${product.imageFilename}"/>"
+							alt="image" style="width: 50%" /></td>
 						<td>${product.name}</td>
 						<td>${product.category}</td>
 						<td>${product.price}</td>
 						<td>${product.manufacturer}</td>
 						<td>${product.unitInStock}</td>
 						<td>${product.description}</td>
+						<td><a
+							href="<spring:url value="/admin/productInventory/editProduct/${product.id}"/>">
+								<span class="glyphicon glyphicon-pencil"></span>
+						</a> <a
+							href="<spring:url value="/admin/productInventory/deleteProduct/${product.id}"/>">
+								<span class="glyphicon glyphicon-remove"></span>
+						</a></td>
 					</tr>
 				</c:forEach>
 			</tbody>
 		</table>
+		<a href="<c:url value="/admin/productInventory/addProduct"/>"
+			class="btn btn-primary"> Add Product </a>
 	</div>
 </div>
